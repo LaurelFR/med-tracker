0568b0ca82d89ce097290e79dc09a3992e2a9a7e
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-10-17 19:03:30 -0600

    Added basic setup and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import MedTracker from "./MedTracker";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="text-center mt-4">Medication Tracker</h1>
+        <MedTracker />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/MedTracker.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function MedTracker() {
+  return (
+    <div className="MedTracker">
+      <div className="mt-4 d-flex justify-content-evenly">
+        <h2>Exact Time</h2>
+        <h2>Relative Time</h2>
+      </div>
+      <div>
+        <ul>
+          <li>Medication</li>
+          <li>Medication</li>
+          <li>Medication</li>
+          <li>Medication</li>
+          <li>Medication</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
