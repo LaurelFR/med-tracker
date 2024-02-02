df2227cedd05f9e393e6b03fc0d815544676c05e
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2024-02-02 14:15:54 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="text-center mt-4">Medication Tracker</h1>
+        <h1 className="text-center mt-4v">Medication Tracker</h1>
         <MedTracker />
       </div>
     </div>
